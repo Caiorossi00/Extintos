95c85e98f0511065c7a463c8e56df5fccbd5bcdb
Caiorossi00 <caio_rossi56@hotmail.com>
2025-01-15 10:44:51 -0300

    estrutura dos times + fotos + swiper

--- src/Routes/Managers/ManagerSlider.js
+++ src/Routes/Managers/ManagerSlider.js
@@ -1,45 +1,98 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import React from "react";
 import teams from "../../assets/data/teams.js";
-import "../../assets/scss/ManagerSlider.scss";
+import "swiper/css";
+import styled from "styled-components";
+
+const ManagerSliderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0;
+
+  h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+  .swiper {
+    width: 100%;
+    max-width: 1200px; // Ajuste conforme necessário
+    margin: 0 auto;
+    min-height: 400px; // Adicionando uma altura mínima para garantir o layout correto
+  }
+`;
+
+const ManagerInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  .manager-image {
+    min-width: 400px;
+    min-height: 400px;
+    max-height: 400px;
+    border-radius: 40px;
+    object-fit: contain;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  h3 {
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 15px;
+    color: #333;
+  }
+
+  p {
+    font-size: 16px;
+    margin-top: 5px;
+    color: #666;
+  }
+`;
 
 export default function ManagerSlider() {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const navigate = useNavigate();
-
-  const nextManager = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % teams.length);
-  };
-
-  const prevManager = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + teams.length) % teams.length
-    );
-  };
 
   const handleManagerClick = (id) => {
     navigate(`/team/${id}`);
   };
 
-  const currentManager = teams[currentIndex];
-
   return (
-    <div>
+    <ManagerSliderWrapper>
       <h2>Team Managers</h2>
-      <div className="manager-slider">
-        <button onClick={prevManager}>Previous</button>
-        <div className="manager-info">
-          <img
-            src={currentManager.managerPicture}
-            alt={currentManager.name}
-            className="manager-image"
-            onClick={() => handleManagerClick(currentManager.id)}
-          />
-          <h3>{currentManager.name}</h3>
-          <p>{currentManager.team}</p>
-        </div>
-        <button onClick={nextManager}>Next</button>
-      </div>
-    </div>
+      <Swiper spaceBetween={50} slidesPerView={1} navigation loop>
+        {teams.map((team) => (
+          <SwiperSlide key={team.id}>
+            <ManagerInfo>
+              <img
+                src={team.managerPicture}
+                alt={team.manager}
+                className="manager-image"
+                onClick={() => handleManagerClick(team.id)}
+              />
+              <h3>{team.manager}</h3>
+            </ManagerInfo>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </ManagerSliderWrapper>
   );
 }
--- src/assets/data/teams.js
+++ src/assets/data/teams.js
@@ -1,43 +1,164 @@
-import igorAmaral from "../images/igorAmaral.jpeg";
-import jacome from "../images/jacome.jpeg";
-import flavio from "../images/flavio.jpeg";
-import jefferson from "../images/jefferson.jpeg";
+import AndrePicolli from "../images/AndrePicolli.jpeg";
+import BrunoTeixeira from "../images/BrunoTeixeira.jpeg";
+import DaniloHara from "../images/DaniloHara.jpeg";
+import EderMurilo from "../images/EderMurilo.jpeg";
+import FlavioTarsis from "../images/FlavioTarsis.jpeg";
+import GabrielSorin from "../images/GabrielSorin.jpeg";
+import GustavoLopes from "../images/GustavoLopes.jpeg";
+import IgorAmaral from "../images/IgorAmaral.jpeg";
+import JeffersonCorrea from "../images/JeffersonCorrea.jpeg";
+import JeffreyRadetski from "../images/JeffreyRadetski.jpeg";
+import JulioCesar from "../images/JulioCesar.jpeg";
+import JuniorJacome from "../images/JuniorJacome.jpeg";
 
 const teams = [
   {
     id: 1,
-    name: "Time Flavio",
+    name: "Manchester United",
     badge: "/assets/images/badge.svg",
-    estadio: "Estádio dos Bagres",
-    manager: "Igor Amaral",
-    managerPicture: flavio,
-    players: ["Jogador 1", "Jogador 2", "Jogador 3", "Jogador 4"],
+    estadio: "Old Trafford",
+    manager: "Andre Picolli",
+    managerPicture: AndrePicolli,
+    players: [
+      "Bruno Fernandes",
+      "Marcus Rashford",
+      "Casemiro",
+      "Harry Maguire",
+    ],
   },
   {
     id: 2,
-    name: "Time Igor",
+    name: "Liverpool FC",
     badge: "/assets/images/badge.svg",
-    estadio: "Estádio dos Bagres",
-    manager: "Igor Amaral",
-    managerPicture: igorAmaral,
-    players: ["Jogador 1", "Jogador 2", "Jogador 3", "Jogador 4"],
+    estadio: "Anfield",
+    manager: "Bruno Teixeira",
+    managerPicture: BrunoTeixeira,
+    players: [
+      "Mohamed Salah",
+      "Virgil van Dijk",
+      "Alisson Becker",
+      "Trent Alexander-Arnold",
+    ],
   },
   {
     id: 3,
-    name: "Bagres Futebol Clube",
+    name: "Manchester City",
     badge: "/assets/images/badge.svg",
-    estadio: "Estádio dos Bagres",
-    manager: "Igor Amaral",
-    managerPicture: jacome,
-    players: ["Jogador 1", "Jogador 2", "Jogador 3", "Jogador 4"],
+    estadio: "Etihad Stadium",
+    manager: "Danilo Hara",
+    managerPicture: DaniloHara,
+    players: [
+      "Kevin De Bruyne",
+      "Erling Haaland",
+      "Phil Foden",
+      "Jack Grealish",
+    ],
   },
   {
     id: 4,
-    name: "Teste",
+    name: "Chelsea FC",
+    badge: "/assets/images/badge.svg",
+    estadio: "Stamford Bridge",
+    manager: "Eder Murilo",
+    managerPicture: EderMurilo,
+    players: ["Kai Havertz", "Mason Mount", "N'Golo Kante", "Thiago Silva"],
+  },
+  {
+    id: 5,
+    name: "Arsenal FC",
+    badge: "/assets/images/badge.svg",
+    estadio: "Emirates Stadium",
+    manager: "Flavio Tarsis",
+    managerPicture: FlavioTarsis,
+    players: [
+      "Bukayo Saka",
+      "Martin Ødegaard",
+      "Gabriel Jesus",
+      "William Saliba",
+    ],
+  },
+  {
+    id: 6,
+    name: "Tottenham Hotspur",
+    badge: "/assets/images/badge.svg",
+    estadio: "Tottenham Hotspur Stadium",
+    manager: "Gabriel Sorin",
+    managerPicture: GabrielSorin,
+    players: [
+      "Harry Kane",
+      "Son Heung-min",
+      "Richarlison",
+      "Pierre-Emile Højbjerg",
+    ],
+  },
+  {
+    id: 7,
+    name: "Leicester City",
+    badge: "/assets/images/badge.svg",
+    estadio: "King Power Stadium",
+    manager: "Gustavo Lopes",
+    managerPicture: GustavoLopes,
+    players: [
+      "James Maddison",
+      "Harvey Barnes",
+      "Youri Tielemans",
+      "Kasper Schmeichel",
+    ],
+  },
+  {
+    id: 8,
+    name: "Burnley FC",
+    badge: "/assets/images/badge.svg",
+    estadio: "Turf Moor",
+    manager: "Igor Amaral",
+    managerPicture: IgorAmaral,
+    players: ["Wout Weghorst", "Maxwel Cornet", "Ashley Barnes", "Jack Cork"],
+  },
+  {
+    id: 9,
+    name: "Everton FC",
     badge: "/assets/images/jefferson-team-badge.svg",
+    estadio: "Goodison Park",
+    manager: "Jefferson Correa",
+    managerPicture: JeffersonCorrea,
+    players: [
+      "Richarlison",
+      "Dominic Calvert-Lewin",
+      "Seamus Coleman",
+      "Jordan Pickford",
+    ],
+  },
+  {
+    id: 10,
+    name: "Wolverhampton Wanderers",
+    badge: "/assets/images/badge.svg",
+    estadio: "Molineux Stadium",
+    manager: "Jeffrey Radetski",
+    managerPicture: JeffreyRadetski,
+    players: ["Raul Jiménez", "Pedro Neto", "Ruben Neves", "João Moutinho"],
+  },
+  {
+    id: 11,
+    name: "Southampton FC",
+    badge: "/assets/images/badge.svg",
+    estadio: "St. Mary's Stadium",
+    manager: "Julio Cesar",
+    managerPicture: JulioCesar,
+    players: [
+      "James Ward-Prowse",
+      "Che Adams",
+      "Theo Walcott",
+      "Mohammed Salisu",
+    ],
+  },
+  {
+    id: 12,
+    name: "Norwich City",
+    badge: "/assets/images/jefferson-team-badge.svg",
+    estadio: "Carrow Road",
     manager: "Júnior Jacome",
-    managerPicture: jefferson,
-    players: ["Caio", "Lucas", "Rossi", "Teste"],
+    managerPicture: JuniorJacome,
+    players: ["Teemu Pukki", "Milot Rashica", "Ben Gibson", "Kenny McLean"],
   },
 ];
 
